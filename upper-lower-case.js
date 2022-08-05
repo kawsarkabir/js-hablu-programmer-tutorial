d037557fd839d1dd6de08d69dedd408b4c0ddc96
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 17:57:22 +0600

    uppercase lowercase done

---
+++ upper-lower-case.js
@@ -0,0 +1,6 @@
+// toUpperCase
+var rochona = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ratione nam facilis dignissimos quis. Omnis fuga quasi temporibus laborum minima modi vero! Delectus iure ducimus eligendi sapiente pariatur hic praesentium."
+console.log(rochona.toUpperCase());
+// tolowerCase
+var lowertext = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint ratione nam facilis dignissimos quis. Omnis fuga quasi temporibus laborum minima modi vero! Delectus iure ducimus eligendi sapiente pariatur hic praesentium'
+console.log(lowertext.toLowerCase())
