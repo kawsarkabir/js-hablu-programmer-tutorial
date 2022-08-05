afa1fe49db295e3d2672bb2ef80b9f035bacfdcd
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 20:28:34 +0600

    operator done

---
+++ operator.js
@@ -0,0 +1,30 @@
+// Jok kora
+var num1 = 100;
+var num2 = 300;
+console.log(num1 + num2);
+
+
+// বিয়োগ করা
+var num1 = 20;
+var num2 = 5;
+console.log(num1-num2);
+
+
+// গুন করা 
+var num1 = 100;
+var num2 = 5;
+console.log(num1*num2);
+
+
+// ভাগ করা
+var num1 = 200;
+var num2 = 100;
+var total = num1 / num2;
+console.log(total);
+
+
+// modules
+var num1 = 500;
+var num2= 100;
+total = num1 % num2;
+console.log(total);
