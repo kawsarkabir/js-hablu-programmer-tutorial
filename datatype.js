40aac7189052cf21a5e4511163fbf9e8d1e473cf
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 17:45:01 +0600

    data type done

---
+++ datatype.js
@@ -0,0 +1,14 @@
+// variable in this data type
+// number
+var myAge = 17;
+console.log(myAge);
+console.log(typeof myAge);
+// string type
+var myName = "KAWSAR KABIR";
+console.log(myName);
+console.log(typeof myName);
+// bolean type
+var iSingle = true;
+console.log(iSingle);
+console.log(typeof iSingle);
+// 
