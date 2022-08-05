01f70b83dcf5d2cb40e72ebc2a67326a8b24c79a
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 20:18:49 +0600

    parsefloat-int ses

---
+++ parsefloarint.js
@@ -0,0 +1,12 @@
+// parsefloat 
+var num1 = "20";
+var num2 = 80;
+num1 = parseFloat(num1);
+var jokGol = num1 + num2;
+console.log(jokGol);
+// parseint
+var num1 = "20.47803";
+var num2 = 80;
+num1 = parseInt(num1);
+var jokGol = num1 + num2;
+console.log(jokGol);
