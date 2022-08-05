0061a18ef3cc9a2aec79f70d7f5b4c6dec278ce9
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 22:28:23 +0600

    if else done

---
+++ condition.js
@@ -0,0 +1,16 @@
+// if condition
+var a = 100;
+var b = 50;
+if( a > b){
+    console.log( "ami paruti khabo");
+}
+// else condition
+
+var a = 1400;
+var b = 900;
+if(a < b){
+    console.log("ami biye korbo");
+}
+else{
+    console.log("ami fokir");
+}
---
+++ predifind-function.js
@@ -0,0 +1,4 @@
+var num1 = 20;
+var num2 = 10;
+var total = num1 + num2;
+console.log(total);
