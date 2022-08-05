9f79c84c09f35f0b22317e18932c2633414839eb
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-05 18:17:57 +0600

    split-indexOF example

---
+++ spilit-indexof.js
@@ -0,0 +1,7 @@
+// .split ar bebohar
+var myfd = "hridoy, miraz, antor, anamul";
+console.log(myfd);
+console.log(myfd.split("hridoy"));
+// indexof ar bebohar
+var nameKoi = "miraz, clkawsar, shawonvai";
+console.log(nameKoi.indexOf("k")); 
