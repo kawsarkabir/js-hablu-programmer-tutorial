e9cfa3924ffdad4d5f327a77ba7edc5ca4267ef8
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-06 17:13:23 +0600

    splice slice

--- array.js
+++ array.js
@@ -2,11 +2,24 @@
 var std = ["kawsar", "antor", "hasan","masan"];
 std.push("tasu");
 console.log(std);
+
+
 // pop
 std.pop();
 console.log(std);
+
+
 // shift
 std.shift();
 console.log(std);
 std.unshift("janu");
 console.log(std);
+
+
+// slice
+console.log(std.slice(0,2));
+std.unshift("kawsar");
+console.log(std);
+
+// splice
+console.log(std.splice(0,2));
