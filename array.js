61849324f26993d90a5ec2f33c89e64c7ce7881e
kawsarkabir <kawsarkabir11@gmail.com>
2022-08-06 16:41:06 +0600

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+// push=====
+var std = ["kawsar", "antor", "hasan","masan"];
+std.push("tasu");
+console.log(std);
+// pop
+std.pop();
+console.log(std);
+// shift
+std.shift();
+console.log(std);
+std.unshift("janu");
+console.log(std);
